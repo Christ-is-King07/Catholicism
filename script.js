074974b56861c8710550e35b6e4c38e0ec1f865b
hypermarce <ayroutmarcelino@gmail.com>
2024-10-02 17:15:11 +1000

    search, optimisation

--- script.js
+++ script.js
@@ -20,34 +20,34 @@ document.addEventListener('DOMContentLoaded', function () {
     function navigateToPage(query) {
         const pages = {
             "saints": "../Saints/saints.html",
-                "st charbel": "../../../Catholicism/Saints/Saint Charbel/Saints/saint_charbel.html",
-                    "saint charbel": "../../../Catholicism/Saints/Saint Charbel/Saints/saint_charbel.html",
-                    "charbel": "../../../Catholicism/Saints/Saint Charbel/Saints/saint_charbel.html",
+                "st charbel": "../Saints/saint_charbel.html",
+                    "saint charbel": "../Saints/saint_charbel.html",
+                    "charbel": "../Saints/saint_charbel.html",
 
-                "padre pio": "../../../Catholicism/Saints/Padre Pio/padre_pio.html",
-                    "saint padre pio": "../../../Catholicism/Saints/Padre Pio/padre_pio.html",
-                    "pio": "../../../Catholicism/Saints/Padre Pio/padre_pio.html",
-                    "padre": "../../../Catholicism/Saints/Padre Pio/padre_pio.html",
+                "padre pio": "../Saints/padre_pio.html",
+                    "saint padre pio": "../Saints/padre_pio.html",
+                    "pio": "../Saints/padre_pio.html",
+                    "padre": "../Saints/padre_pio.html",
 
-                //"st anthony of padua": "../../../Catholicism/Saints/St Anthony of Padua/st_anthony_of_padua.html",
-                    //"st anthony": "../../../Catholicism/Saints/St Anthony of Padua/st_anthony_of_padua.html",
-                    //"saint anthony": "../../../Catholicism/Saints/St Anthony of Padua/st_anthony_of_padua.html",
-                    //"anthony": "../../../Catholicism/Saints/St Anthony of Padua/st_anthony_of_padua.html",
+                "st anthony of padua": "../Saints/st_anthony_of_padua.html",
+                    "st anthony": "../Saints/st_anthony_of_padua.html",
+                    "saint anthony": "../Saints/st_anthony_of_padua.html",
+                    "anthony": "../Saints/st_anthony_of_padua.html",
 
-            "prayers": "../../../Catholicism/Prayers/prayers.html",
-                "rosary": "../../../Catholicism/Prayers/Rosary/rosary.html",
-                "nicene creed": "../../../Catholicism/Prayers/Nicene Creed/nicene_creed.html",
+            "prayers": "../Prayers/prayers.html",
+                "rosary": "../Prayers/rosary.html",
+                "nicene creed": "../Prayers/nicene_creed.html",
 
-            //"papacy": "../../../Catholicism/Papacy/papacy.html",
-                //"history of catholic hierarchy": "../../../Catholicism/Papacy/History of Catholic Hierarchy/history_of_catholic_hierarchy.html",
-                //"line of popes": "../../../Catholicism/Papacy/Line of Popes/line_of_popes.html",
-                //"popes": "../../../Catholicism/Papacy/Line of Popes/line_of_popes.html",
+            "papacy": "../Papacy/papacy.html",
+                 "history of catholic hierarchy": "../Papacy/history_of_catholic_hierarchy.html",
+                "line of popes": "../Papacy/line_of_popes.html",
+                "popes": "../Papacy/line_of_popes.html",
 
-            "the assumption": "../../../Catholicism/Catholicism/Assumption/assumption.html",
-                "assumption": "../../../Catholicism/Catholicism/Assumption/assumption.html",
-            "confession": "../../../Catholicism/Catholicism/Confession/confession.html",
-            "eucharist": "../../../Catholicism/Catholicism/Eucharist/eucharist.html",
-            "glossary": "../../../Catholicism/Catholicism/Glossary/glossary.html"
+            "the assumption": "../Catholicism/assumption.html",
+                "assumption": "../Catholicism/assumption.html",
+            "confession": "../Catholicism/confession.html",
+            "eucharist": "../Catholicism/eucharist.html",
+            "glossary": "../Catholicism/glossary.html"
         };
 
         // Navigate to the page if the query matches
